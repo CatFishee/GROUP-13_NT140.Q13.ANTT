88d64603c87f93f91afce5a5d8b4e75d440b005c
CatFishe <23521176@gm.uit.edu.vn>
2025-10-14 15:18:15 +0700

    worm folder added

--- Worm/Properties/AssemblyInfo.cs
+++ Worm/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Downloader Trojan")]
+[assembly: AssemblyTitle("Worm")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Downloader Trojan")]
+[assembly: AssemblyProduct("Worm")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("819862fe-1f24-45fc-9e99-83549cacf3f0")]
+[assembly: Guid("1eb30399-7cb6-4204-8a34-82a163b4e3ac")]
 
 // Version information for an assembly consists of the following four values:
 //
